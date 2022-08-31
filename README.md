ea6958d73e1bbc29cec238f44781248cdcc06618
Rodrigo Rocha <rodrigorgs@gmail.com>
2022-08-31 15:20:36 -0300

    Adiciona script de instalação

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # lab-scripts
+
+Para instalar o script de atualização na máquina, execute o seguinte comando:
+
+```
+wget -O - https://github.com/graco-ufba/lab-scripts/blob/main/install.sh | /bin/bash
+```
+
+O script `lab-startup.sh` será executado a cada inicialização da máquina.
+
+Para visualizar o log, execute:
+
+```
+journalctl -fu labstartup.service
+```
---
+++ install.sh
@@ -0,0 +1,23 @@
+echo '#!/bin/sh
+wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-startup.sh -O /tmp/startup.sh
+chmod a+x /tmp/startup.sh
+/tmp/startup.sh
+exit 0
+' > /root/labstartup.sh
+chmod a+x /root/labstartup.sh
+
+echo '[Unit]
+Description=Atualiza instalacao dos labs
+Wants=network-online.target
+After=network.target network-online.target
+
+[Service]
+ExecStart=/root/labstartup.sh
+Type=oneshot
+
+[Install]
+WantedBy=multi-user.target
+' > /etc/systemd/system/labstartup.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable labstartup.service
