849bfca54e289337b363e208e25c0c638a52eef3
Rodrigo Rocha <rodrigorgs@gmail.com>
2023-07-07 14:10:44 -0300

    Desativa IPv6

--- lab-startup.sh
+++ lab-startup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#########
+# Desativa IPv6
+
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
 ############### Criação do usuário aluno ###############
 
 # Pula configuração do Ubuntu pelo usuário novo
