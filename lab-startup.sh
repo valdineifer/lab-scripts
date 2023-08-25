58fce1b5f3f6c49a555eba2e1a566058f11f5ac4
Rodrigo Rocha <rodrigorgs@gmail.com>
2023-08-25 17:27:45 -0500

    Update lab-startup.sh

--- lab-startup.sh
+++ lab-startup.sh
@@ -61,7 +61,7 @@ if [[ "$USER" == "aluno" ]]; then
   	echo export PATH="/opt/android-studio/bin:/opt/Android/Sdk/platform-tools:\$PATH" >> /home/aluno/.bashrc
   	rm -f /opt/flutter/bin/cache/lockfile
 
-   	ln -s /opt/gradle .gradle
+   	ln -s /opt/gradle /home/$USER/.gradle
         
         echo "DROP USER IF EXISTS '\''aluno'\''@'\''localhost'\''; CREATE USER '\''aluno'\''@'\''%'\'' IDENTIFIED BY '\''aluno'\''; GRANT ALL PRIVILEGES ON *.* TO '\''aluno'\''@'\''%'\'';" | mysql
         sudo -u postgres dropdb --if-exists aluno; sudo -u postgres createdb aluno
