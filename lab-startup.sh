022eb3d742bf6e3a8603f486771de911c89568a1
bertolima <bertolilima27@gmail.com>
2024-04-18 16:20:23 -0300

    corrigido .profile

--- lab-startup.sh
+++ lab-startup.sh
@@ -41,10 +41,37 @@ gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'sh
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 7200
 " > /etc/skel/set-wallpaper.sh
 chmod +x /etc/skel/set-wallpaper.sh
-echo "~/set-wallpaper.sh" >> /etc/skel/.profile
+echo "# ~/.profile: executed by the command interpreter for login shells.
+# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
+# exists.
+# see /usr/share/doc/bash/examples/startup-files for examples.
+# the files are located in the bash-doc package.
+
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+#umask 022
+
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+	. "$HOME/.bashrc"
+    fi
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+~/set-wallpaper.sh" > /etc/skel/.profile
 
 if ! [ -f /etc/profile.d/autologout.sh ]; then
-rm -f /etc/profile.d/autologout.sh
+sudo rm -f /etc/profile.d/autologout.sh
 fi
 
 # Pula configuração do Ubuntu pelo usuário novo
