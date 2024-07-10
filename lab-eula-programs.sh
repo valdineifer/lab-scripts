5da01c9c55db49e50d20a37477174d42bcc6eddb
Luís Bertoli <104996143+bertolima@users.noreply.github.com>
2024-07-10 12:40:11 -0300

    virtualbox-ext-pack

--- lab-eula-programs.sh
+++ lab-eula-programs.sh
@@ -7,11 +7,9 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install mokutil
 SBDISABLED=$(mokutil --sb)
 if [ "$SBDISABLED" = "SecureBoot disabled" ]; then
-    sudo apt-get install virtualbox
-    if [ -f /home/luis/Downloads/Linux.ova -a ! -d /opt/Linux ]; then
-        vboxmanage import /home/luis/Downloads/Linux.ova --vsys 0 --basefolder "/opt"
-	chown -R aluno:aluno /opt/Linux
-    fi
+    sudo apt install virtualbox
+    echo virtualbox-ext-pack virtualbox-ext-pack/license select "true" | sudo debconf-set-selections
+    sudo apt install virtualbox-ext-pack
 fi
 
 #wireshark
@@ -28,15 +26,11 @@ sudo chmod +x /usr/bin/dumpcap
 fi
 
 #CiscoPacketTracer
-
 if ! [ -d /opt/pt ]; then
 wget "https://www.dropbox.com/scl/fi/fpexy62c8mybh10k1u5h1/Packet_Tracer822_amd64_signed.deb?rlkey=1hvmun574d7p6ud4ey32xwl2j&st=6dnoy373&dl=1" -O /opt/cisco.deb
-
 cd /opt
-
 echo PacketTracer PacketTracer_822_amd64/accept-eula select "true" | sudo debconf-set-selections
 echo PacketTracer PacketTracer_822_amd64/show-eula select "false" | sudo debconf-set-selections
-
 DEBIAN_FRONTEND=noninteractive dpkg -i cisco.deb
 sudo apt install -f -y
 DEBIAN_FRONTEND=noninteractive dpkg -i cisco.deb
@@ -45,7 +39,6 @@ sudo wget "https://drive.google.com/uc?export=download&id=1L01Mg96hWRpeI9LNOqzug
 cd /etc/skel
 sudo unzip pt.zip
 sudo rm /etc/skel/pt.zip
-
 fi
 
 
