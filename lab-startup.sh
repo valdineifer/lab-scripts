a392cd56973c36bdc0d1a84609497b3cee353951
bertolima <bertolilima27@gmail.com>
2024-07-11 17:42:50 -0300

    alterando para diretorio correto na main

--- lab-startup.sh
+++ lab-startup.sh
@@ -2,10 +2,10 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/alpha/lab-profile-config.sh -O /tmp/lab-profile-config.sh
-wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/alpha/lab-aluno-config.sh -O /tmp/lab-aluno-config.sh
-wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/alpha/lab-programs.sh -O /tmp/lab-programs.sh
-wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/alpha/lab-eula-programs.sh -O /tmp/lab-eula-programs.sh
+wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-profile-config.sh -O /tmp/lab-profile-config.sh
+wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-aluno-config.sh -O /tmp/lab-aluno-config.sh
+wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-programs.sh -O /tmp/lab-programs.sh
+wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-eula-programs.sh -O /tmp/lab-eula-programs.sh
 
 if ! [ -f /usr/local/sbin/done.txt ]; then
 	sudo touch /usr/local/sbin/done.txt
